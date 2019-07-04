8812576b0fba9c7766b5abd39ba68c805f2ddfcd
Dushantha <dushantha.ekanayake@mosaic.com>
2019-07-04 09:19:54 -0400

    modified fish config

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,10 @@ set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_paths
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
+set -g theme_color_scheme solarized-dark
+set -g theme_newline_cursor yes
+set -g theme_title_display_user yes
+set -g theme_title_display_process yes
+set -g theme_display_cmd_duration yes
+set -g theme_nerd_fonts yes
+
